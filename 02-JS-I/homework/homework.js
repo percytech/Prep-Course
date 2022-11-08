3acae02e394b135d7be91d417f445bc9a4642886
tpercy <tonnypercy@gmail.com>
2022-11-07 22:13:46 -0500

    test JSIV corregido

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -224,7 +224,7 @@ function deEuroAdolar(euro){
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
 
-  var dolar = euro * 1.20;
+  let dolar = euro * 1.20;
 
   return dolar; 
 }
--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -181,7 +181,7 @@ function doWhile(numero) {
   //Retornar el valor final.
   //Usar el bucle do ... while.
 
-  var i = 0;
+  let i = 0;
   do {
     i = i + 1;
     numero = numero + 5;
